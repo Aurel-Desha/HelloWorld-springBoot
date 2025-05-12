e704ba8e7a0d9172f86088cbbf0007c9231bafdc
aurel-shael <deshyesguy2@gmail.com>
2025-05-12 18:28:53 +0200

    workflow

--- src/main/java/org/yourmedia/HelloWorldApplication.java
+++ src/main/java/org/yourmedia/HelloWorldApplication.java
@@ -1,22 +1,27 @@
 package org.yourmedia;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class HelloWorldApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
-    @RequestMapping("/")
-    public String helloWorld() {
-        return "Hello World, We are YourMedia";
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloWorldApplication.class);
     }
 
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World, We are YourMedia";
+    }
 }
